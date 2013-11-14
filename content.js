a4de27a04b7849516e50973542ef40ce568a72f8
Flávio Juvenal <flaviojuvenal@gmail.com>
2013-11-14 20:22:45 -0300

    Matching function

--- content.js
+++ content.js
@@ -1,5 +1,19 @@
+var regex = new RegExp('http(?:s)?://content.bitsontherun.com/players/(.*)-(?:.*).js', "i");
+
 function findJwScripts() {
-  return [1,2,3];
+  var scripts = document.querySelectorAll('script');
+  var result = [];
+
+  for (var i = 0; i < scripts.length; i++) {
+    var s = scripts[i];
+
+    var match = null;
+    if (s.src && (match = regex.exec(s.src))) {
+      result.push(match[1]);
+    }
+  }
+  
+  return result;
 }
 
 chrome.runtime.sendMessage({jwScripts: findJwScripts()}, function (response) {});
